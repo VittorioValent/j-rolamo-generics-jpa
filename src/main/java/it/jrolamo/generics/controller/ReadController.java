c2abb84f88eecce00c20b58576e1693cb8543141
VittorioValent <v.valent@contrader.it>
2021-05-24 11:45:35 +0200

    fixed read mapping

--- src/main/java/it/jrolamo/generics/controller/ReadController.java
+++ src/main/java/it/jrolamo/generics/controller/ReadController.java
@@ -1,13 +1,16 @@
 package it.jrolamo.generics.controller;
 
+import javax.websocket.server.PathParam;
+
 import com.querydsl.core.types.Predicate;
-import it.jrolamo.generics.domain.AbstractDTO;
-import it.jrolamo.generics.service.ICrudService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import it.jrolamo.generics.domain.AbstractDTO;
+import it.jrolamo.generics.service.ICrudService;
 
 /**
  * This Controller handles all read-only requests from client.
@@ -44,8 +47,8 @@ public abstract class ReadController<DTO extends AbstractDTO> {
      * @param id
      * @return
      */
-    @GetMapping("/read")
-    public DTO read(@RequestParam Long id) {
+    @GetMapping("/read/{id}")
+    public DTO read(@PathParam(value = "id") Long id) {
         return service.read(id);
     }
 }
